dbef122c5e63f60ecd56d1a7bd6a23c7caacb0a3
SANTIAGO AGUSTIN ABBRACCIAVENTO <42224885@ort>
2018-11-14 20:35:11 -0300

    Se sube proyecto MVC

---
+++ FutboleAR/Models/CanchaModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FutboleAR.Models
+{
+    public class CanchaModels
+    {
+        public String CanchaNombre { get; set; }
+        public String Direccion { get; set; }
+        public int Capacidad { get; set; }
+    }
+}
---
+++ FutboleAR/Models/MatchModels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FutboleAR.Models
+{
+    public class MatchModels
+    {
+        public String Nombre { get; set; }
+        public DateTime FechaHora { get; set; }
+        //public List<CanchaModels> Cancha { get; set; }
+        public CanchaModels cancha { get; set; }
+    }
+}
